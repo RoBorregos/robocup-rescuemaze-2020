93818be87b6351e02a00e2812f93c5cbeee2852e
Eme112 <eme_pedraza@hotmail.com>
2020-03-01 21:47:47 -0600

    Corrected and improved

--- VirtualTest/Test2/Test2.ino
+++ VirtualTest/Test2/Test2.ino
@@ -1,358 +1,147 @@
 #include "Tile.h"
-#include "TVector.h"
 #include "Map.h"
-
-uint8_t iSize = 11, iRow = 1, iColumn = 1;
-char virtualMap[11][11] = {{'.','W','.','W','.','W','.','W','.','W','.'},
-                    {'W','R','W',' ','W',' ','.',' ','.',' ','W'},
-                    {'.','.','.','.','.','.','.','.','.','.','.'},
-                    {'W',' ','.',' ','W',' ','W',' ','W',' ','W'},
-                    {'.','.','.','.','.','.','.','.','.','.','.'},
-                    {'W',' ','W',' ','W',' ','W',' ','W',' ','W'},
-                    {'.','.','.','.','.','W','.','.','.','.','.'},
-                    {'W',' ','.',' ','W',' ','W',' ','W',' ','W'},
-                    {'.','.','.','.','.','.','.','.','.','.','.'},
-                    {'W',' ','W',' ','.',' ','.',' ','.',' ','W'},
-                    {'.','W','.','W','.','W','.','W','.','W','.'}};
-
-void printVirtualMap() {
-  for(int iX = 0 ; iX < iSize ; iX++) {
-    for(int iY = 0 ; iY < iSize ; iY++) {
-      Serial.print(virtualMap[iX][iY]);
-      if(iY < iSize - 1) Serial.print(" ");
-    }
-    Serial.println("");
-  }
-}
-
-void moveMapNorth() {
-  virtualMap[iRow][iColumn] = 'V';
-  virtualMap[iRow - 2][iColumn] = 'R';
-  iRow -= 2;
-}
-
-void moveMapEast() {
-  virtualMap[iRow][iColumn] = 'V';
-  virtualMap[iRow][iColumn + 2] = 'R';
-  iColumn += 2;
-}
-
-void moveMapSouth() {
-  virtualMap[iRow][iColumn] = 'V';
-  virtualMap[iRow + 2][iColumn] = 'R';
-  iRow += 2;
-}
-
-void moveMapWest() {
-  virtualMap[iRow][iColumn] = 'V';
-  virtualMap[iRow][iColumn - 2] = 'R';
-  iColumn -= 2;
-}
-
-void printMap(Map tiles_map, const uint8_t iFloor) {
-  char char_map[tiles_map.getRows()*2 + 1][tiles_map.getColumns()*2 + 1];
-  int row_char, column_char;
-
-  for(int i = 0; i < tiles_map.getRows()*2 + 1; ++i) {
-    for(int j = 0; j < tiles_map.getColumns()*2 + 1; ++j) {
-      char_map[i][j] = '.';
-    }
-  }
-  
-  row_char = 1;
-
-  for (int row_tile = 0 ; row_tile < tiles_map.getRows() ; row_tile++) {
-    column_char = 1;
-    for (int column_tile = 0 ; column_tile < tiles_map.getColumns() ; column_tile++) {
-      if(tiles_map.getTile(row_tile, column_tile).getStart()) {
-        char_map[row_char][column_char] = 'S';
-      }
-      else if(tiles_map.getTile(row_tile, column_tile).getVisited()) {
-        char_map[row_char][column_char] = 'V';
-      }
-      else if(tiles_map.getTile(row_tile, column_tile).getAccessible()) {
-        char_map[row_char][column_char] = ' ';
-      }
-      else {
-        char_map[row_char][column_char] = 'X';
-      }
-      if(tiles_map.getTile(row_tile, column_tile).getNorth()) {
-        char_map[row_char - 1][column_char] = '.';
-      }
-      else {
-        char_map[row_char - 1][column_char] = 'W';
-      }
-      if(tiles_map.getTile(row_tile, column_tile).getEast()) {
-        char_map[row_char][column_char + 1] = '.';
-      }
-      else {
-        char_map[row_char][column_char + 1] = 'W';
-      }
-      if(tiles_map.getTile(row_tile, column_tile).getSouth()) {
-        char_map[row_char + 1][column_char] = '.';
-      }
-      else {
-        char_map[row_char+1][column_char] = 'W';
-      }
-      if(tiles_map.getTile(row_tile, column_tile).getWest()) {
-        char_map[row_char][column_char - 1] = '.';
-      }
-      else {
-        char_map[row_char][column_char - 1] = 'W';
-      }
-      column_char += 2;
-    }
-    row_char += 2;
-  }
-  
-  for(int i = 0 ; i < tiles_map.getRows()*2 + 1 ; i++) {
-    for(int j = 0 ; j < tiles_map.getColumns()*2 + 1 ; j++) {
-      Serial.print(char_map[i][j]);
-      Serial.print(" ");
-    }
-    Serial.println(" ");
-  }
-  Serial.println(" ");
-}
-
-struct coord {
-  uint8_t x, y;
-};
-
-struct dijkstra_tile {
-  uint8_t weight;
-  coord prev, current;
-};
-
-TVector<char> dijkstra(Map tiles_map) {
-  TVector<coord*> open_vector, non_visited_vector;
-  TVector<char> path;
-  dijkstra_tile matrix[tiles_map.getRows()][tiles_map.getColumns()];
-  coord *current_coord;
-  uint8_t current_row = tiles_map.getCurrRow();
-  uint8_t current_column = tiles_map.getCurrColumn();
-
-  for (int i = 0 ; i < tiles_map.getRows() ; ++i) {
-    for (int j = 0 ; j < tiles_map.getColumns() ; ++j) {
-      matrix[i][j].current.x = i;
-      matrix[i][j].current.y = j;
-      matrix[i][j].weight = 255;
-      
-      if (tiles_map.getTile(i, j).getAccessible() == true && tiles_map.getTile(i, j).getVisited() == false) {
-        current_coord = &matrix[i][j].current;
-        non_visited_vector.push(current_coord);
-      }
-      
-    }
-  }
-
-  matrix[current_row][current_column].weight = 0;
-  current_coord = &matrix[current_row][current_column].current;
-  open_vector.push(current_coord);
-
-  while (open_vector.getSize() > 0) {
-    current_coord = open_vector[0];
-    
-    if (tiles_map.getTile(current_coord->x, current_coord->y).getNorth() == true) {
-      if (matrix[current_coord->x - 1][current_coord->y].weight > matrix[current_coord->x - 1][current_coord->y].weight + tiles_map.getTile(current_coord->x - 1, current_coord->y).getWeight()) {
-        matrix[current_coord->x - 1][current_coord->y].weight = matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x - 1, current_coord->y).getWeight();
-        matrix[current_coord->x - 1][current_coord->y].prev = matrix[current_coord->x][current_coord->y].current;
-      }
-    }
-    if (tiles_map.getTile(current_coord->x, current_coord->y).getEast() == true) {
-      if (matrix[current_coord->x][current_coord->y + 1].weight > matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x, current_coord->y + 1).getWeight()) {
-        matrix[current_coord->x][current_coord->y + 1].weight = matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x, current_coord->y + 1).getWeight();
-        matrix[current_coord->x][current_coord->y + 1].prev = matrix[current_coord->x][current_coord->y].current;
-      }
-    }
-    if (tiles_map.getTile(current_coord->x, current_coord->y).getSouth() == true) {
-      if (matrix[current_coord->x + 1][current_coord->y].weight > matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x + 1, current_coord->y).getWeight()) {
-        matrix[current_coord->x + 1][current_coord->y].weight = matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x + 1, current_coord->y).getWeight();
-        matrix[current_coord->x + 1][current_coord->y].prev = matrix[current_coord->x][current_coord->y].current;
-      }
-    }
-    if (tiles_map.getTile(current_coord->x, current_coord->y).getWest() == true) {
-      if (matrix[current_coord->x][current_coord->y - 1].weight > matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x, current_coord->y - 1).getWeight()) {
-        matrix[current_coord->x][current_coord->y - 1].weight = matrix[current_coord->x][current_coord->y].weight + tiles_map.getTile(current_coord->x, current_coord->y - 1).getWeight();
-        matrix[current_coord->x][current_coord->y - 1].prev = matrix[current_coord->x][current_coord->y].current;
-      }
-    }
-    
-  }
-
-  if (non_visited_vector.getSize() > 0) current_coord = non_visited_vector[0];
-  else Serial.print("No hay tiles por recorrer");
-
-  for (int i = 1 ; i < non_visited_vector.getSize() ; ++i) {
-    if (matrix[current_coord->x][current_coord->y].weight > matrix[non_visited_vector[i]->x][non_visited_vector[i]->y].weight) {
-      current_coord = non_visited_vector[i];
-    }
-  }
-
-  while (current_coord->x != current_row || current_coord->y != current_column) {
-    if (current_coord->x - matrix[current_coord->x][current_coord->y].prev.x == -1) {
-      path.pushFront('N');
-    }
-    if (current_coord->y - matrix[current_coord->x][current_coord->y].prev.y == 1) {
-      path.pushFront('E');
-    }
-    if (current_coord->x - matrix[current_coord->x][current_coord->y].prev.x == 1) {
-      path.pushFront('S');
-    }
-    if (current_coord->y - matrix[current_coord->x][current_coord->y].prev.y == -1) {
-      path.pushFront('W');
-    }
-  }
-
-  return path;
-}
+#include "TVector.h"
+#include "Dijkstra.h"
+#include "VirtualFunctions.h"
 
 void setup() {
-  Serial.begin(9600);
-  printVirtualMap();
-  Tile currTile;
-  currTile.setVisited();
-  currTile.setAccessible();
+  Tile current_tile;
   TVector<char> path;
-  unsigned short int current_row = 0, current_column = 0;
+  unsigned short int current_row = 0, current_column = 0, current_zone = 0;
 
-  if (virtualMap[0][1] != 'W') {
-    currTile.setNorth();
-  }
-  if (virtualMap[1][2] != 'W') {
-    currTile.setEast();
-  }
-  if (virtualMap[2][1] != 'W') {
-    currTile.setSouth();
-  }
-  if (virtualMap[1][0] != 'W') {
-    currTile.setWest();
-  }
+  Serial.begin(9600);
 
-  Map tiles_map(currTile);
-  current_row = 0;
-  current_column = 0;
-
-  //creating new adjacent tiles
-  if (tiles_map.currTile().getNorth() == true) {
-    Tile newTile;
-    newTile.setSouth();
-    newTile.setAccessible();
-    tiles_map.addRowFirst(newTile);
+  // Updating walls of the first tile.
+  if (virtual_map[0][1] != 'W') {
+    current_tile.setNorth();
   }
-  if (tiles_map.currTile().getEast() == true) {
-    Tile newTile;
-    newTile.setWest();
-    newTile.setAccessible();
-    tiles_map.addColumnLast(newTile);
+  if (virtual_map[1][2] != 'W') {
+    current_tile.setEast();
   }
-  if (tiles_map.currTile().getSouth() == true) {
-    Tile newTile;
-    newTile.setNorth();
-    newTile.setAccessible();
-    tiles_map.addRowLast(newTile);
+  if (virtual_map[2][1] != 'W') {
+    current_tile.setSouth();
   }
-  if (tiles_map.currTile().getWest() == true) {
-    Tile newTile;
-    newTile.setEast();
-    newTile.setAccessible();
-    tiles_map.addColumnFirst(newTile);
+  if (virtual_map[1][0] != 'W') {
+    current_tile.setWest();
   }
 
-  printMap(tiles_map, 1);
-  //tiles_map.print();
-  Serial.println("");
-  Serial.println("");
-  delay(2000);
-
-  path = dijkstra(tiles_map);
-
-  while (tiles_map.getUnvisitedTiles() != 0) {
+  // Setting values of the first tile.
+  current_tile.setVisited();
+  current_tile.setAccessible();
+  current_tile.setStart();
   
+  // Creating the map with the initial zone.
+  Map zone0;
+  zone0.setInitialTile(current_tile);
+  TVector<Map> tiles_map;
+  tiles_map.pushAsFirst(zone0);
+
+  // Creating and adding new adjacent tiles.
+  if (tiles_map[current_zone].currTile().ableToGoNorth() == true) {
+    Tile new_tile;
+    new_tile.setSouth();
+    new_tile.setAccessible();
+    //if (virtual_map[][] == 'B') new_tile.setBlack();
+    //tiles_map[current_zone].addRowFirst(new_tile);
+  }
+  if (tiles_map[current_zone].currTile().ableToGoEast() == true) {
+    Tile new_tile;
+    new_tile.setWest();
+    new_tile.setAccessible();
+    if (virtual_map[1][3] == 'B') new_tile.setBlack();
+    tiles_map[current_zone].addColumnLast(new_tile);
+  }
+  if (tiles_map[current_zone].currTile().ableToGoSouth() == true) {
+    Tile new_tile;
+    new_tile.setNorth();
+    new_tile.setAccessible();
+    if (virtual_map[3][1] == 'B') new_tile.setBlack();
+    tiles_map[current_zone].addRowLast(new_tile);
+  }
+  if (tiles_map[current_zone].currTile().ableToGoWest() == true) {
+    Tile new_tile;
+    new_tile.setEast();
+    new_tile.setAccessible();
+    //if (virtual_map[][] == 'B') new_tile.setBlack();
+    //tiles_map[current_zone].addColumnFirst(new_tile);
+  }
+
+  printVirtualMap();
+  Serial.println("");
+  printMap(tiles_map[current_zone], 1);
+  
+  ///////////////////////////////////////////////////////////////////
+  /////////////////////////// MAIN LOOP /////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  while (tiles_map[current_zone].getUnvisitedTiles() != 0) {
+    Dijkstra dijkstra_matrix(tiles_map[current_zone]);
+    path = dijkstra_matrix.getPath(tiles_map[current_zone]);
+    //path = dijkstra(tiles_map[current_zone]);
+    Serial.println(path.getSize());
+
+    /////////////////////////////////////////////
+    for (int i = 0 ; i < path.getSize() ; ++i) {
+      Serial.print(path[i]);
+      Serial.print(" ");
+    }
+    /////////////////////////////////////////////
+    Serial.println("");
+    Serial.println("");
+    Serial.println("");
+    delay(100);
+
+    tiles_map[current_zone] = followPath(path, tiles_map[current_zone]);
+
+    tiles_map[current_zone] = updateTiles(tiles_map[current_zone]);
+
+    printMap(tiles_map[current_zone], 1);
+    Serial.println("");
+    //////////////////////////////////////////////
+    Serial.print("UNVISITED TILES: ");
+    Serial.println(tiles_map[current_zone].getUnvisitedTiles());
+    //////////////////////////////////////////////
+  }
+  ///////////////////////////////////////////////////////////////////
+  //////////////////////////// END LOOP /////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+
+  
+  /*path = goToStart(tiles_map[current_zone]);
+
+  /////////////////////////////////////////////
+  for (int i = 0 ; i < path.getSize() ; ++i) {
+    Serial.print(path[i]);
+    Serial.print(" ");
+  }
+  /////////////////////////////////////////////
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
+  delay(100);
+
+  while (path.getSize() > 0) {
+    if (path[0] == 'N') {
+      tiles_map[current_zone].moveNorth();
+      moveMapNorth();
+    }
+    else if (path[0] == 'E') {
+      tiles_map[current_zone].moveEast();
+      moveMapEast();
+    }
+    else if (path[0] == 'S') {
+      tiles_map[current_zone].moveSouth();
+      moveMapSouth();
+    }
+    else if (path[0] == 'W') {
+      tiles_map[current_zone].moveWest();
+      moveMapWest();
+    }
     
-
-    currTile = tiles_map.currTile();
-    currTile.setVisited();
-    
-    //Update North
-    if (virtualMap[tiles_map.getCurrRow()*2][tiles_map.getCurrColumn()*2 + 1] != 'W') {
-      currTile.setNorth();
-      if (tiles_map.getCurrRow() > 0 && tiles_map.northTile().getVisited() == false) {
-        Tile northTile = tiles_map.northTile();
-        northTile.setSouth();
-        northTile.setAccessible();
-        tiles_map.setTile(northTile, 'N');
-      }
-      else if (tiles_map.getCurrRow() == 0) {
-        Tile northTile;
-        northTile.setSouth();
-        northTile.setAccessible();
-        tiles_map.addRowFirst(northTile);
-      }
-    }
-
-    //Update East
-    if (virtualMap[tiles_map.getCurrRow()*2 + 1][tiles_map.getCurrColumn()*2 + 2] != 'W') {
-      currTile.setEast();
-      if (tiles_map.getCurrColumn() < tiles_map.getColumns() - 1 && tiles_map.eastTile().getVisited() == false) {
-        Tile eastTile = tiles_map.eastTile();
-        eastTile.setWest();
-        eastTile.setAccessible();
-        tiles_map.setTile(eastTile, 'E');
-      }
-      else if (tiles_map.getCurrColumn() == tiles_map.getColumns() - 1) {
-        Tile eastTile;
-        eastTile.setWest();
-        eastTile.setAccessible();
-        tiles_map.addColumnLast(eastTile);
-      }
-    }
-
-    //Update South
-    if (virtualMap[tiles_map.getCurrRow()*2 + 2][tiles_map.getCurrColumn()*2 + 1] != 'W') {
-      currTile.setSouth();
-      if (tiles_map.getCurrRow() < tiles_map.getRows() - 1 && tiles_map.southTile().getVisited() == false) {
-        Tile southTile = tiles_map.southTile();
-        southTile.setNorth();
-        southTile.setAccessible();
-        tiles_map.setTile(southTile, 'S');
-      } 
-      else if (tiles_map.getCurrRow() == tiles_map.getRows() - 1) {
-        Tile southTile;
-        southTile.setNorth();
-        southTile.setAccessible();
-        tiles_map.addRowLast(southTile);
-      }
-    }
-
-    //Update West
-    if (virtualMap[tiles_map.getCurrRow()*2 + 1][tiles_map.getCurrColumn()*2] != 'W') {
-      currTile.setWest();
-      if (tiles_map.getCurrColumn() > 0 && tiles_map.westTile().getVisited() == false)  {
-        Tile westTile = tiles_map.westTile();
-        westTile.setEast();
-        westTile.setAccessible();
-        tiles_map.setTile(westTile, 'W');
-      }
-      else if (tiles_map.getCurrColumn() == 0) {
-        Tile westTile;
-        westTile.setEast();
-        westTile.setAccessible();
-        tiles_map.addColumnFirst(westTile);
-      }
-    }
-
-    tiles_map.setTile(currTile, '0');
-
-    Serial.print("Orientation: ");
-    Serial.println(tiles_map.getOrientation());
     printVirtualMap();
     Serial.println("");
-    printMap(tiles_map, 1);
-    //tiles_map.print();
-    Serial.println("");
-    delay(2000);
-  }
+
+    path.popFirst();
+  }*/
 }
 
 void loop() {
